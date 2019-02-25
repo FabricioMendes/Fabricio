89850a4d7a27fea558309a1d89d922fa4db14169
Zebraico <47994791+Zebraico@users.noreply.github.com>
2019-02-25 19:55:18 -0300

    Aula Design
    teste upload

---
+++ C return 1;.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int A=5;
+	int B=6;
+	if(A>B)
+	{
+		printf("A e maior que B!\n");
+	}
+	else
+	{
+		if (A<B)
+		{
+			printf("B e maior que A! \n");
+		}
+		else
+		{
+			printf("B e igual a A!\n");
+		}
+	}
+	
+	return 1;
+}
